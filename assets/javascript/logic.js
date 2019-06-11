a558fadffd2ad9faa630d377869e22e14006bb8f
Harrison, Rachel <harrison.e.rachel@gmail.com>
2019-06-10 20:41:31 -0500

    initial js push

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    var addTrain = $(this).attr()
+
+// submit button on click function
+$("#add-train").on("click", function (event) {
+    // press enter or submit
+    event.preventDefault();
+    // grab input from textbox
+    // var addTrain = $(".input-group-prepend").val().trim();
+    // $("input-group-prepend").val("");
+}
